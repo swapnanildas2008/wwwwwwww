e8d21534381b6dde0abded8f23874e7ea3353a17
swapnanildas2008 <73822779+swapnanildas2008@users.noreply.github.com>
2021-02-12 10:25:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function adduser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1", player1_name);
+    localStorage.setItem("player2", player2_name);
+    window.location = "index1.html";
+}
+function send(){
+    score = localStorage.getItem("player");
+    document.getElementById("update").innerHTML = "<h1>score:"+score+"</h1>";
+}
